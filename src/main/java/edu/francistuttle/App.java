38fb550ae9451a929c9b504b392fc64d3b6c3d1b
CameronWiggins <cw1101046@student.francistuttle.edu>
2023-11-30 10:05:37 -0600

    first commit

---
+++ src/main/java/edu/francistuttle/App.java
@@ -0,0 +1,44 @@
+package edu.francistuttle;
+
+import com.sun.syndication.feed.synd.SyndCategoryImpl;
+import com.sun.syndication.feed.synd.SyndContentImpl;
+import com.sun.syndication.feed.synd.SyndEntry;
+import com.sun.syndication.feed.synd.SyndFeed;
+import com.sun.syndication.feed.synd.SyndLinkImpl;
+import com.sun.syndication.io.SyndFeedInput;
+import com.sun.syndication.io.XmlReader;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        try {
+            File folder = new File("C:\\Users\\cw1101046\\Desktop\\Github\\nasaparser\\src\\main\\java\\edu\\francistuttle\\nasa.xml");
+            
+            SyndFeedInput input = new SyndFeedInput();
+            SyndFeed feed = input.build(new XmlReader(folder));
+
+            System.out.println("Feed Title: " + feed.getTitle());
+            System.out.println("Feed Link: " + feed.getLink());
+            System.out.println("Feed Description: " + feed.getDescription());
+
+            for (SyndEntry entry : (List<SyndEntry>) feed.getEntries())
+            {
+                System.out.println("\tEntry Title: " + entry.getTitle());
+                System.out.println("\tEntry Link: " + entry.getLink());
+                System.out.println("\tEntry Description: " + entry.getDescription());
+            }
+
+        } catch (Exception e) {
+            // TODO: handle exception
+
+        }
+    }
+}
